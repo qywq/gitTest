e9b803f4554f92747708b3f37750aa76cbeb1e71
wyq <1176519796@qq.com>
2022-10-01 00:31:05 +0800

    second commit

--- src/main/java/com/arguigu/git/GitTest.java
+++ src/main/java/com/arguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.arguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git！");
     }
 }
